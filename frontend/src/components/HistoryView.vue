2871708c9374097f5c23b93ab82d3edf90291262
Martin Falkmer <noobgubbe@live.se>
2023-09-01 20:31:32 +0200

    Rebuild history view

--- frontend/src/components/HistoryView.vue
+++ frontend/src/components/HistoryView.vue
@@ -1,26 +1,57 @@
 <template>
-    <table class="container table table-striped table-dark table-hover">
-      <thead>
-        <tr>
-          <th>Match ID</th>
-          <th>Team Kills</th>
-          <th>Stats</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="match in this.matches" :key="match.MatchID">
-          <td>{{ match.MatchID }}</td>
-          <td>{{ match.TeamKills }}</td>
-          <td>
-            <HistoryPlayerStats :players="match.players"/>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div v-for="match in this.matches" :key="match.id" class="container">
+      <table role="button" data-bs-toggle="modal" data-bs-target="#match_modal" class="table table-sm table-hover rounded" style="overflow: hidden">
+        <thead class="table-light">
+          <tr>
+            <th>SummonerName</th>
+            <th>Kills</th>
+            <th>Deaths</th>
+            <th>Assists</th>
+            <th>KDA</th>
+            <th>Kill Participation</th>
+            <th>Push Ups</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="player in match.players" :key="player.id">
+            <td>{{ player.SummonerName }}</td>
+            <td class="text-success">{{ player.Kills }}</td>
+            <td class="text-danger">{{ player.Deaths }}</td>
+            <td class="text-warning">{{ player.Assists }}</td>
+            <td>{{ format_number(player.KDA) }}</td>
+            <td>{{ format_number(player.KillParticipation * 100) }}%</td>
+            <td class="text-info">{{ player.PushUps }}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="modal" id="match_modal">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <table class="table table-borderless table-sm table-hover rounded" style="overflow: hidden; margin: 0">
+              <thead class="table-light">
+                <tr>
+                  <th>Game ID</th>
+                  <th>Team Kills</th>
+                  <th>Minimum Push Ups</th>
+                  <th>Maximum Push Ups</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{{ match.MatchID }}</td>
+                  <td>{{ match.TeamKills }}</td>
+                  <td>{{ match.MinPushUps }}</td>
+                  <td>{{ match.MaxPushUps }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
-import HistoryPlayerStats from './HistoryPlayerStats.vue';
 
 export default {
     name: 'HistoryView',
@@ -36,10 +67,12 @@ export default {
         async getData() {
             this.matches = await this.backend_client
               .get("matches")
-              .then(response => response.data);
+              .then(response => response.data.reverse());
         },
+        format_number(number) {
+          return parseFloat(number).toFixed(2)
+        }
     },
-    components: { HistoryPlayerStats }
 }
 
 
