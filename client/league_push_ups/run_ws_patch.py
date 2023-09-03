8d54caeed5e44968ffc540ff686da1e564f38758
Martin Falkmer <noobgubbe@live.se>
2023-09-03 13:21:34 +0200

    updated run_ws_patch

--- client/league_push_ups/run_ws_patch.py
+++ client/league_push_ups/run_ws_patch.py
@@ -9,26 +9,24 @@ async def run_ws(self: Connection) -> None:
 
     :return: None
     """
-    local_session = aiohttp.ClientSession(auth=aiohttp.BasicAuth('riot', self._auth_key), # pylint: disable=protected-access
-                                            headers={'Content-Type': 'application/json',
-                                                    'Accept': 'application/json'})
-    self._ws = await local_session.ws_connect(self.ws_address, ssl=False)                 # pylint: disable=protected-access
-    await self._ws.send_json([5, 'OnJsonApiEvent'])                                       # pylint: disable=protected-access
-    _ = await self._ws.receive()                                                          # pylint: disable=protected-access
+    async with aiohttp.ClientSession(
+        auth=aiohttp.BasicAuth('riot', self._auth_key), # pylint: disable=protected-access
+        headers={'Content-Type': 'application/json', 'Accept': 'application/json'}
+    ) as session:
+        async with session.ws_connect(self.ws_address, ssl=False) as ws:
+            await ws.send_json([5, 'OnJsonApiEvent'])
+            await ws.receive()
 
-    while self.closed is False:
-        await asyncio.sleep(0)
-        msg = await self._ws.receive()                                                    # pylint: disable=protected-access
-        logger.debug('Websocket frame received')
+            async for msg in ws:
+                await asyncio.sleep(0)
+                logger.debug('Websocket frame received')
 
-        if msg.type == aiohttp.WSMsgType.TEXT:
-            try:
-                data = loads(msg.data)[2]
-                self._connector.ws.match_event(self._connector, self, data)               # pylint: disable=protected-access
-            except JSONDecodeError:
-                logger.warning('Error decoding the following JSON: %s', msg.data)
+                if msg.type == aiohttp.WSMsgType.TEXT:
+                    try:
+                        data = loads(msg.data)[2]
+                        self._connector.ws.match_event(self._connector, self, data)               # pylint: disable=protected-access
+                    except JSONDecodeError:
+                        logger.warning('Error decoding the following JSON: %s', msg.data)
 
-        elif msg.type == aiohttp.WSMsgType.CLOSED:
-            break
-    await self._ws.close()                                                                # pylint: disable=protected-access
-    await local_session.close()
+                elif msg.type == aiohttp.WSMsgType.CLOSED:
+                    break
