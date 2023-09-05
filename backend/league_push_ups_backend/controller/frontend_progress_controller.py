e1e7a9120149f00ec3288ce9061d6e008579386e
Martin Falkmer <noobgubbe@live.se>
2023-09-05 13:35:29 +0200

    mypy, pylint

--- backend/league_push_ups_backend/controller/frontend_progress_controller.py
+++ backend/league_push_ups_backend/controller/frontend_progress_controller.py
@@ -9,21 +9,21 @@ from ..models.database.user.summoner import Summoner
 from ..models.database.session import Session
 
 class FrontendProgressController(Controller):
-    def post(self) -> list[dict[str, Any]]:
+    def post(self) -> dict[str, list[Any]]:
         filters = request.get_json()
         assert isinstance(filters, dict)
         matches = MatchPlayer.select(
             User.username,
-            Session.id.alias("SessionID"),
+            Session.id.alias("SessionID"), # type: ignore[attr-defined] # pylint: disable=no-member
             Session.date_time.cast("char").alias("SessionDateTime"),
             Match.date_time.cast("char").alias("MatchDateTime"),
             Match.MatchID,
             MatchPlayer.Kills.alias("Kills"),
             MatchPlayer.Deaths.alias("Deaths"),
             MatchPlayer.Assists.alias("Assists"),
-            MatchPlayer.kda.cast("float").alias("KDA"),
-            MatchPlayer.kill_participation.cast("float").alias("KillParticipation"),
-            MatchPlayer.push_ups.cast("int").alias("PushUps"),
+            MatchPlayer.kda.cast("float").alias("KDA"), # type: ignore[attr-defined] # pylint: disable=no-member
+            MatchPlayer.kill_participation.cast("float").alias("KillParticipation"), # type: ignore[attr-defined] # pylint: disable=no-member
+            MatchPlayer.push_ups.cast("int").alias("PushUps"), # type: ignore[attr-defined] # pylint: disable=no-member
             ).join(
                 Match
             ).join(
@@ -36,6 +36,10 @@ class FrontendProgressController(Controller):
                 User
             ).dicts()
         return {
-            match["username"]: [nested_match for nested_match in matches if nested_match["username"] == match["username"]]
+            match["username"]: [
+                nested_match
+                for nested_match in matches
+                if nested_match["username"] == match["username"]
+            ]
             for match in matches
         }
