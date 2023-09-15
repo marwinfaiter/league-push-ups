18759c86aeb8dd446282942904e00fe961eb30cf
Martin Falkmer <noobgubbe@live.se>
2023-09-15 16:01:33 +0200

    Added developmentproxy for frontend

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,5 +3,11 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     allowedHosts: "all",
+    proxy: {
+      '/api': {
+        target: 'http://backend:5000',
+        pathRewrite: {"^/api": ""}
+      },
+    },
   }
 })
