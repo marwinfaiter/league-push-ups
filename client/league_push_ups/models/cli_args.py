1bcb07c7a9ae23bd14d1e525bb51d4006961006c
Marwin Faiter <noobgubbe@live.se>
2023-10-14 13:41:15 +0200

    Added prompt for username and password

--- client/league_push_ups/models/cli_args.py
+++ client/league_push_ups/models/cli_args.py
@@ -1,6 +1,16 @@
 from tap import Tap
+from getpass import getpass
 
 class CLIArgs(Tap):
-    username: str
-    password: str
+    username: str = ""
+    password: str = ""
     backend_url: str = "https://api.leaguepushups.buddaphest.se"
+
+    def parse_args(self, *args, **kwargs) -> "CLIArgs": # type: ignore[no-untyped-def,override]
+        cli_args = super().parse_args(*args, **kwargs)
+        if cli_args.username == "":
+            cli_args.username = input("Username: ").strip()
+
+        if cli_args.password == "":
+            cli_args.password = getpass()
+        return cli_args
